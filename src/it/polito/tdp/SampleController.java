55ec9c1ed07c0ac24fe5984b2bdafed1d904193c
Pietro <Pietro@Pcstudio>
2017-03-19 20:54:21 +0100

    buttonnn

--- src/it/polito/tdp/SampleController.java
+++ src/it/polito/tdp/SampleController.java
@@ -1,5 +1,19 @@
 package it.polito.tdp;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
 public class SampleController {
-	
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
 }
